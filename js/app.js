1651d90ea87923d10ef26458c7660e0e0d14eef5
cristianCeamatu <cristian.ceamatu@gmail.com>
2020-04-24 20:07:34 +0300

    Pevented default events when clickin on dropdown menu buttons

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 "use strict";
 
-document.querySelector("#apps").addEventListener("click", () => {
+document.querySelector("#apps").addEventListener("click", (event) => {
+    event.preventDefault();
     if (document.querySelector(".nav-account").classList.contains("show")) 
     document.querySelector(".nav-account").classList.remove("show");
     document.querySelector(".nav-apps").classList.toggle("show");
 })
 
-document.querySelector("#account").addEventListener("click", () => {
+document.querySelector("#account").addEventListener("click", (event) => {
+    event.preventDefault();
     if (document.querySelector(".nav-apps").classList.contains("show")) 
         document.querySelector(".nav-apps").classList.remove("show");
     document.querySelector(".nav-account").classList.toggle("show");
